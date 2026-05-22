c33493d01e4091c02e93587903ae0c020fc125ec
Jakob <zippererjakob@gmail.com>
2026-05-22 13:15:20 +0200

    initial commit

---
+++ 02_code/shell/init_project.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir -p 00_docs/literature
+mkdir -p 01_data/raw
+mkdir -p 01_data/processed
+mkdir -p 02_code/R
+mkdir -p 02_code/python
+mkdir -p 02_code/shell
+mkdir -p 03_report/graphs
+mkdir -p 03_report/tables
+
+touch 00_docs/literature/.gitkeep
+touch 01_data/raw/.gitkeep
+touch 02_code/R/.gitkeep
+touch 02_code/python/.gitkeep
+touch 02_code/shell/.gitkeep
+touch 03_report/graphs/.gitkeep
+touch 03_report/tables/.gitkeep
